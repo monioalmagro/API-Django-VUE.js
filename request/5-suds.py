fdcf94af03f34da15ccad4a6298ebb552d5cb032
emi <emazzurque@gmail.com>
2020-05-26 00:57:22 -0300

    libreria suds

--- request/5-suds.py
+++ request/5-suds.py
@@ -4,6 +4,8 @@ from suds.client import Client
   
 #Instantiate a new Client Object
 client = Client("https://cvnet.cpd.ua.es/servicioweb/publicos/pub_gestdocente.asmx?wsdl")
-print client
+#print client
+print client.last_received()
+
 #Call the desired method using the service instance variable
 #client.service.MyMethod(myArgument)
